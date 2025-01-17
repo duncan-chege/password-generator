d52372c2323f57afe4152eba69a5ed59a7e9feee
viisualworks <dshege4@gmail.com>
2025-01-17 12:00:45 +0300

    add a password input field

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import PasswordGenerator from './components/PasswordGenerator'
 
 function App() {
   return (
-    <>
+    <div className='bg-very-dark-grey min-h-screen grid grid-cols-1 justify-items-center'>
       <PasswordGenerator />
-    </>
+    </div>
   )
 }
 
--- src/components/PasswordGenerator.tsx
+++ src/components/PasswordGenerator.tsx
@@ -1,8 +1,21 @@
 const PasswordGenerator = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="w-1/4">
+      <h1 className="text-xl text-grey font-bold text-center pt-32 pb-8">
+        Password Generator
+      </h1>
+
+      <form className="">
+        <div className="relative">
+          <input
+            type="text"
+            name="password"
+            className="bg-grey/50 text-almost-white p-2 outline-none w-full"
+          />
+          <img src="../assets/copy-icon.svg" alt="" className="absolute bottom-3 right-4 w-4" />
+        </div>
+      </form>
+    </div>
   );
 };
 
