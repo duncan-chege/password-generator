7e4728798300db74be630ac553e26d411fcca334
viisualworks <dshege4@gmail.com>
2025-01-24 14:43:25 +0300

    change the main role

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import PasswordGenerator from "./components/PasswordGenerator";
 function App() {
   return (
     <div className="bg-very-dark-grey min-h-screen grid grid-cols-1 justify-items-center content-center bg-wave bg-no-repeat bg-cover">
-      <div role="main">
         <PasswordGenerator />
-      </div>
     </div>
   );
 }
